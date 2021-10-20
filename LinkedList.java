134d89989c9941864e3dbfaac798880c2caf1587
rombirli <rombirli@gmail.com>
2021-10-20 13:55:55 +0200

    message

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+//https://www.geeksforgeeks.org/linked-list-set-1-introduction/
+class LinkedList{
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+        }
+    }
+}
+
